af620dd65cc7c150ffc9094aaf3ad0f5aa0de2f2
apontes <pontesaguinaldo15@gmail.com>
2016-05-23 20:28:49 +0100

    basic word guessing

--- hangman.js
+++ hangman.js
@@ -1,37 +1,46 @@
-
 const regex = new RegExp("[A-z]");
 const stripRegex = /\s/g;
 const letterRegex = /\s/i;
 
 class Hangman {
-    constructor(){
+    constructor() {
         this.maxGuesses = 6;
         this.running = false;
     }
 
-    guess(letter){
-        if(!regex.test(letter)) {
+    guess(letter) {
+        if (!regex.test(letter)) {
             return "Note that you don't need to guess special characters or numbers";
         }
 
         letter = letter.toUpperCase();
+//TODO change how correct word check is done.
+        if (!this.guesses[letter]) {
+            if (letter.length > 1) {
+                stripRegex.lastIndex = 0;
+                let stripped = letter.toUpperCase().replace(stripRegex, "");
+                if (stripped === this.strippedWord) {
+                    this.running = false;
+                    return `Congratulations! The word was "${this.word}".`;
+                }
+            }
 
-        if(!this.guesses[letter]) {
             let guess = this.guesses[letter] = {
                 letter: letter,
                 correct: this.wordUpper.indexOf(letter) !== -1
             };
-            if(!guess.correct)
+
+            if (!guess.correct)
                 this.nGuesses++;
 
             let representation = this.representation;
-            if(representation.length === this.strippedWord.length) {
+            if (representation.length === this.strippedWord.length) {
                 this.running = false;
                 return `Congratulations! The word was "${this.word}".`;
             } else {
                 let result = guess.correct ? "Correct" : "Incorrect";
                 let extra = "";
-                if(this.nGuesses >= this.maxGuesses)
+                if (this.nGuesses >= this.maxGuesses)
                     extra = "- The chat failed! It's possible to quit using !quit";
 
                 return `${result}! [${representation}] - ${this.nGuesses}/${this.maxGuesses} - ${this.allGuesses} ${extra}`;
@@ -42,12 +51,12 @@ class Hangman {
         }
     }
 
-    get representation(){
+    get representation() {
         let ret = "";
-        for(let i = 0; i < this.wordUpper.length; ++i){
+        for (let i = 0; i < this.wordUpper.length; ++i) {
             let letter = this.word[i];
             let guess = this.guesses[this.wordUpper[i]];
-            if(!regex.test(letter) && !letterRegex.test(letter) || guess && guess.correct)
+            if (!regex.test(letter) && !letterRegex.test(letter) || guess && guess.correct)
                 ret += letter;
         }
 
@@ -57,9 +66,9 @@ class Hangman {
     get allGuesses() {
         let right = [];
         let wrong = [];
-        for(let key of Object.keys(this.guesses)) {
+        for (let key of Object.keys(this.guesses)) {
             let g = this.guesses[key];
-            if(g.correct)
+            if (g.correct)
                 right.push(g.letter);
             else
                 wrong.push(g.letter);
@@ -67,12 +76,13 @@ class Hangman {
 
         return `Right: ${right.join()} Wrong: ${wrong.join()}`;
     }
+
     start(word) {
         this.word = word;
         this.wordUpper = word.toUpperCase();
         this.guesses = {};
         this.nGuesses = 0;
-        this.strippedWord = this.word.replace(stripRegex, "");
+        this.strippedWord = this.wordUpper.replace(stripRegex, "");
         this.running = true;
 
         return `A new Hangman game has started! [${this.representation}] ${this.nGuesses}/${this.maxGuesses}.`;
@@ -84,4 +94,4 @@ class Hangman {
     }
 }
 
-module.exports  = Hangman;
+module.exports = Hangman;
